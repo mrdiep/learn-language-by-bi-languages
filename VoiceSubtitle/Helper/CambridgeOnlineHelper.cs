6deba2496372542e1c4ad9a83c88a0dae6b5b52b
Điệp Nguyễn Văn <diep.nguyen@adslot.com>
2016-05-07 08:11:34 +0700

    Code

---
+++ VoiceSubtitle/Helper/CambridgeOnlineHelper.cs
@@ -0,0 +1,132 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using VoiceSubtitle.Model;
+
+namespace VoiceSubtitle.Helper
+{
+    public class CambridgeOnlineHelper
+    {
+        public void CrawToDatabase()
+        {
+            SQLiteConnection connection = new SQLiteConnection(@"Data Source=C:\Users\diepnguyenv\Downloads\anh_viet\anh_viet\anh_viet.db;Version=3;");
+            List<string> words = new List<string>();
+            Regex regex = new Regex(@"^[a-zA-Z]*$");
+            string allword = string.Empty;
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.CommandText = "SELECT `word` FROM anh_viet";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string a = reader[0] as string;
+                        if (regex.Match(a).Success && !(new Regex("^[A-Z]*$").Match(a).Success))
+                        {
+                            words.Add(a);
+                        }
+                    }
+                }
+
+                words = words.Distinct().ToList();
+                allword = string.Join(" ", words);
+            }
+            Task.Factory.StartNew(async () =>
+            {
+               
+                var tasks = new List<Task<WordPronunciation>>();
+               
+                foreach (var word in words)
+                {
+                    if (word.Trim().Length > 0)
+                    {
+                        tasks.Add(DownloadPrononciation(word));
+                    }
+                }
+
+                var listItems = await Task.WhenAll(tasks);
+
+                using (var conn = new SQLiteConnection(@"Data Source=C:\Users\diepnguyenv\Downloads\anh_viet\anh_viet\dict - Copy.db;Version=3;"))
+                {
+                    conn.Open();
+                    using (var cmd = new SQLiteCommand(conn))
+                    {
+                        using (var transaction = conn.BeginTransaction())
+                        {
+                            // 100,000 inserts
+                            foreach (var item in e2)
+                            {
+                                cmd.CommandText =
+                                    "INSERT INTO english (`word`,`uk`,`us`,`ukvoice`,`usvoice`) VALUES (@word,@uk,@us,@ukvoice,@usvoice);";
+
+                                cmd.Parameters.AddWithValue("@word", item.Text);
+                                cmd.Parameters.AddWithValue("@uk", item.UK);
+                                cmd.Parameters.AddWithValue("@us", item.US);
+                                cmd.Parameters.AddWithValue("@ukvoice", item.UKVoiceLink);
+                                cmd.Parameters.AddWithValue("@usvoice", item.USVoiceLink);
+
+                                cmd.ExecuteNonQuery();
+                            }
+
+                            transaction.Commit();
+                        }
+                    }
+                    conn.Close();
+                }
+
+            });
+        }
+
+        private static async Task<WordPronunciation> DownloadPrononciation(string word)
+        {
+            using (var webClient = new WebClient() { Encoding = Encoding.UTF8 })
+            {
+                try
+                {
+                    string url = $@"http://dictionary.cambridge.org/dictionary/english/{HttpUtility.UrlEncode(word.ToLower())}";
+                    var html = new HtmlDocument();
+                    var hString = await webClient.DownloadStringTaskAsync(new Uri(url, UriKind.RelativeOrAbsolute));
+                    html.LoadHtml(hString);
+
+                    var root = html.DocumentNode;
+                    var posHeader = root.Descendants().Where(n => n.GetAttributeValue("class", "").Equals("pos-header")).FirstOrDefault();
+
+                    var audioUk = posHeader?.Descendants("span").Where(x => x.GetAttributeValue("class", "").Equals("uk"))?.FirstOrDefault()
+                        .Descendants().Where(x => x.GetAttributeValue("title", "").Contains("listen to British English pronunciation"))
+                        .FirstOrDefault().GetAttributeValue("data-src-mp3", "");
+
+                    var prononceUk = posHeader?.Descendants("span").Where(x => x.GetAttributeValue("class", "").Equals("uk"))?.Skip(1)
+                        .Select(p => p.Descendants().Where(x => x.GetAttributeValue("class", "").Contains("pron")).FirstOrDefault().InnerText);
+
+                    var audioUs = posHeader?.Descendants("span").Where(x => x.GetAttributeValue("class", "").Equals("us"))?.FirstOrDefault()
+                        .Descendants().Where(x => x.GetAttributeValue("title", "").Contains("listen to American pronunciation"))
+                        .FirstOrDefault().GetAttributeValue("data-src-mp3", "");
+                    var prononceUs = posHeader?.Descendants("span").Where(x => x.GetAttributeValue("class", "").Equals("us"))?.FirstOrDefault()
+                        .Descendants().Where(x => x.GetAttributeValue("class", "").Equals("pron")).FirstOrDefault().InnerText;
+
+                    var wordPronunciation = new WordPronunciation()
+                    {
+                        Text = word,
+                        UK = string.Join(" ", prononceUk),
+                        UKVoiceLink = audioUk,
+                        US = prononceUs,
+                        USVoiceLink = audioUs
+                    };
+
+                    return wordPronunciation;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
--- VoiceSubtitle/ViewModel/CambridgeDictionaryViewModel.cs
+++ VoiceSubtitle/ViewModel/CambridgeDictionaryViewModel.cs
@@ -17,63 +17,22 @@ namespace VoiceSubtitle.ViewModel
     {
         public CambridgeDictionaryViewModel()
         {
-            SQLiteConnection connection = new SQLiteConnection(@"Data Source=C:\Users\diepnguyenv\Downloads\anh_viet\anh_viet\anh_viet.db;Version=3;");
-            List<string> words = new List<string>();
-            Regex regex = new Regex(@"^[a-zA-Z]*$");
-            string allword = string.Empty;
-            using (var command = connection.CreateCommand())
-            {
-                connection.Open();
-                command.CommandText = "SELECT `word` FROM anh_viet";
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        string a = reader[0] as string;
-                        if (regex.Match(a).Success && !(new Regex("^[A-Z]*$").Match(a).Success))
-                        {
-                            words.Add(a);
-                        }
-                    }
-                }
-
-                words = words.Distinct().ToList();
-                allword = string.Join(" ", words.Skip(1000));
-            }
-            Task.Factory.StartNew(() =>
-            {
-                var e2 = Prononce(allword).Result.Where(x=>x!=null);
-
-using (var conn = new SQLiteConnection(@"Data Source=C:\Users\diepnguyenv\Downloads\anh_viet\anh_viet\dict - Copy.db;Version=3;"))
-                {
-                    conn.Open();
-                    using (var cmd = new SQLiteCommand(conn))
-                    {
-                        using (var transaction = conn.BeginTransaction())
-                        {
-                            // 100,000 inserts
-                            foreach(var item in e2) 
-                            {
-                                cmd.CommandText =
-                                    "INSERT INTO english (`word`,`uk`,`us`,`ukvoice`,`usvoice`) VALUES (@word,@uk,@us,@ukvoice,@usvoice);";
-
-                                cmd.Parameters.AddWithValue("@word", item.Text);
-                                cmd.Parameters.AddWithValue("@uk", item.UK);
-                                cmd.Parameters.AddWithValue("@us", item.US);
-                                cmd.Parameters.AddWithValue("@ukvoice", item.UKVoiceLink);
-                                cmd.Parameters.AddWithValue("@usvoice", item.USVoiceLink);
-
-                                cmd.ExecuteNonQuery();
-                            }
-
-                            transaction.Commit();
-                        }
-                    }
-                    conn.Close();
-                }
-
-            });
+            Task.Factory.StartNew(FetchData);
         }
+
+        private void FetchData()
+        {
+
+            using (SQLiteConnection connection  = new SQLiteConnection())
+            {
+                using ( var command  = connection.CreateCommand())
+                {
+                    command.CommandText = "Select * from english";
+                }
+            }
+        }
+
+        protected Dictionary<string, WordPronunciation>   DataRef { get; }
 
         public async Task<WordPronunciation[]> Prononce(string sentence)
         {
@@ -93,49 +52,6 @@ using (var conn = new SQLiteConnection(@"Data Source=C:\Users\diepnguyenv\Downlo
             return listItems;
         }
 
-        private static async Task<WordPronunciation> DownloadPrononciation(string word)
-        {
-            using (var webClient = new WebClient() { Encoding = Encoding.UTF8 })
-            {
-                try
-                {
-                    string url = $@"http://dictionary.cambridge.org/dictionary/english/{HttpUtility.UrlEncode(word.ToLower())}";
-                    var html = new HtmlDocument();
-                    var hString = await webClient.DownloadStringTaskAsync(new Uri(url, UriKind.RelativeOrAbsolute));
-                    html.LoadHtml(hString);
-
-                    var root = html.DocumentNode;
-                    var posHeader = root.Descendants().Where(n => n.GetAttributeValue("class", "").Equals("pos-header")).FirstOrDefault();
-
-                    var audioUk = posHeader?.Descendants("span").Where(x => x.GetAttributeValue("class", "").Equals("uk"))?.FirstOrDefault()
-                        .Descendants().Where(x => x.GetAttributeValue("title", "").Contains("listen to British English pronunciation"))
-                        .FirstOrDefault().GetAttributeValue("data-src-mp3", "");
-
-                    var prononceUk = posHeader?.Descendants("span").Where(x => x.GetAttributeValue("class", "").Equals("uk"))?.Skip(1)
-                        .Select(p => p.Descendants().Where(x => x.GetAttributeValue("class", "").Contains("pron")).FirstOrDefault().InnerText);
-
-                    var audioUs = posHeader?.Descendants("span").Where(x => x.GetAttributeValue("class", "").Equals("us"))?.FirstOrDefault()
-                        .Descendants().Where(x => x.GetAttributeValue("title", "").Contains("listen to American pronunciation"))
-                        .FirstOrDefault().GetAttributeValue("data-src-mp3", "");
-                    var prononceUs = posHeader?.Descendants("span").Where(x => x.GetAttributeValue("class", "").Equals("us"))?.FirstOrDefault()
-                        .Descendants().Where(x => x.GetAttributeValue("class", "").Equals("pron")).FirstOrDefault().InnerText;
-
-                    var wordPronunciation = new WordPronunciation()
-                    {
-                        Text = word,
-                        UK = string.Join(" ", prononceUk),
-                        UKVoiceLink = audioUk,
-                        US = prononceUs,
-                        USVoiceLink = audioUs
-                    };
-
-                    return wordPronunciation;
-                }
-                catch (Exception)
-                {
-                    return null;
-                }
-            }
-        }
+       
     }
 }
